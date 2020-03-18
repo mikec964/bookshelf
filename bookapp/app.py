6e40c8719fe7b3705b78ef73a728bf2e21cbead5
Mike Combs <mike@combsnet.com>
2020-03-18 12:59:18 -0400

    refactor: change to pytest

--- bookapp/app.py
+++ bookapp/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html', title="Home", greeting='Bienvenido')
+    return render_template('home.html', title="Home Page", greeting='Bienvenido')
 
 if __name__ == '__main__':
     app.debug = True
--- bookapp/tests/home_test.py
+++ bookapp/tests/home_test.py
@@ -1,12 +1,22 @@
 from selenium import webdriver
 
-driver = webdriver.Firefox()
+def test_setup():
+    global driver
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(10)
+    driver.maximize_window()
 
-driver.implicitly_wait(10)
-driver.maximize_window()
+def test_home():
+    driver.get("http://localhost:5000/")
+    assert "Home Page" in driver.title
 
-driver.get("http://localhost:5000/")
-assert "Home Page" in driver.title
+def test_greeting():
+    driver.get("http://localhost:5000/")
+    # heading = driver.find_element_by_tag_name("h1").text
+    heading = driver.find_element_by_xpath("//h1[1]").text
+    assert "Flask" in heading
 
-driver.close()
-driver.quit()
+def test_teardown():
+    driver.close()
+    driver.quit()
+
