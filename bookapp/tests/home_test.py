f6017bca680bdf7771b2e96fdeec36bcc301be65
Mike Combs <mike@combsnet.com>
2020-03-18 13:19:06 -0400

    refactor: use test fixture, consolidate tests

--- bookapp/tests/home_test.py
+++ bookapp/tests/home_test.py
@@ -1,22 +1,19 @@
+import pytest
 from selenium import webdriver
 
+@pytest.fixture()
 def test_setup():
     global driver
     driver = webdriver.Firefox()
     driver.implicitly_wait(10)
     driver.maximize_window()
-
-def test_home():
-    driver.get("http://localhost:5000/")
-    assert "Home Page" in driver.title
-
-def test_greeting():
-    driver.get("http://localhost:5000/")
-    # heading = driver.find_element_by_tag_name("h1").text
-    heading = driver.find_element_by_xpath("//h1[1]").text
-    assert "Flask" in heading
-
-def test_teardown():
+    yield
     driver.close()
     driver.quit()
+
+def test_home(test_setup):
+    driver.get("http://localhost:5000/")
+    assert "Home Page" in driver.title
+    # heading = driver.find_element_by_xpath("//h1[1]").text
+    assert "Flask" in driver.find_element_by_tag_name("h1").text
 
