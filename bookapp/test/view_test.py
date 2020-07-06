556b227d62d5951d3f1a40e15ba2b1645f4662a2
Mike Combs <mike@combsnet.com>
2020-07-06 17:58:49 -0400

    fix: load web browser fixture per test

--- bookapp/test/view_test.py
+++ bookapp/test/view_test.py
@@ -2,16 +2,16 @@ import pytest
 from selenium import webdriver
 
 @pytest.fixture()
-def test_setup():
+def web_setup():
     global driver
     driver = webdriver.Firefox()
     driver.implicitly_wait(10)
-    driver.maximize_window()
+#    driver.maximize_window()
     yield
     driver.close()
     driver.quit()
 
-def test_home(test_setup):
+def test_home(web_setup):
     driver.get("http://localhost:5000/")
     assert "Home Page" in driver.title
     # heading = driver.find_element_by_xpath("//h1[1]").text
